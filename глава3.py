898f268d55d2643a8d4e9e8913bb72bd13b70fdf
klinok9 <1.4.hoffer@gmail.com>
2021-03-17 20:46:08 +0300

    work version

--- глава3.py
+++ глава3.py
@@ -14,6 +14,23 @@ import random
 # if random_choice == 0 and player_choice == 0:
 #     print("ничья")
 # elif random_choice
+winner = ''
+player_choice = input('введи: камень, ножницы или бумага: ')
 choices = ['камень', 'ножницы', 'бумага']
-computer_choice = random.choice(choices)  # знакомство с функцией random.choice
-print(computer_choice)
+computer_choice = random.choice(choices) # знакомство с функцией random.choice
+if computer_choice == player_choice:
+    winner = 'ничья!'
+
+elif computer_choice == "бумага" and player_choice == 'камень':
+    winner = 'комп'
+elif computer_choice == "камень" and player_choice == 'ножницы':
+    winner = 'комп'
+elif computer_choice == "ножницы" and player_choice == 'бумага':
+    winner = 'комп'
+else:
+    winner = 'ты'
+if winner == 'ничья!':
+    print('победила дружба')
+else:
+    print('Победил' , winner)
+
